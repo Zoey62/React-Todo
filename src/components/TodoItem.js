cf29960b69a835429a85cd7ee06c5669ace1cd90
zoey <zoey.lee@kaonsoft.com>
2024-06-24 00:41:29 +0900

    done 수정

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -65,10 +65,10 @@ const TodoItem = ({ id, done, text }) => {
     const onRemove = () => dispatch({ type: 'REMOVE', id });
     return (
         <TodoItemBlock>
-            <CheckCircle done={done} onClick={onToggle}>
+            <CheckCircle done={done ? "true" : ""} onClick={onToggle}>
                 {done && <MdDone />}
             </CheckCircle>
-            <Text done={done}>{text}</Text>
+            <Text done={done ? "true" : ""}>{text}</Text>
             <Remove onClick={onRemove}>
                 <MdDelete />
             </Remove>
